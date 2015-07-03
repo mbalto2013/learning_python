1083cf88b7b5bfc42f93cb15d85adc5590e488fd
mbalto2013 <mbmkiller88@gmail.com>
2015-07-03 17:07:49 -0600

    Delete ex1.py

--- ex1.py
+++
@@ -1,7 +0,0 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typin this."
-print "This is fun"
-print "Yay! Printing."
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
