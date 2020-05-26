04f3f0619643dcfab3657ecd6c6871ff0b8e36ba
Milton <mbalto2013@hotmail.com>
2020-05-25 18:00:17 -0600

    new version

--- DataStructures/Queue.py
+++ DataStructures/Queue.py
@@ -5,50 +5,57 @@ class Queue:
     self.head = None
     self.tail = None
     self.max_size = max_size
-    self.size = 0
-    
-  def enqueue(self, value):
+    self.current_size = 0
+  
+  def enqueue(self, node_value):
+    item_to_add = Node(node_value)
     if self.has_space():
-      item_to_add = Node(value)
-      print("Adding " + str(item_to_add.get_node_data()) + " to the queue!")
       if self.is_empty():
         self.head = item_to_add
         self.tail = item_to_add
       else:
         self.tail.set_next_node(item_to_add)
         self.tail = item_to_add
-      self.size += 1
+      self.current_size +=1
+      print(f"Adding: {node_value}")
     else:
-      print("Sorry, no more room!")
-         
+        print('There is no room')
+  
   def dequeue(self):
-    if self.get_size() > 0:
-      item_to_remove = self.head
-      print(str(item_to_remove.get_node_data()) + " is served!")
-      if self.get_size() == 1:
-        self.head = None
-        self.tail = None
-      else:
-        self.head = self.head.get_next_node()
-      self.size -= 1
-      return item_to_remove.get_node_data()
+    if self.get_current_size() > 0:
+        item_to_remove = self.head
+        if self.get_current_size() == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.get_next_node()
+        self.current_size = self.current_size - 1
+        print(f"Removing: {item_to_remove.get_node_data()}")
+        return item_to_remove.get_node_data()
     else:
-      print("The queue is totally empty!")
+        return
+
+  def get_current_size(self):
+    return self.current_size
   
-  def peek(self):
-    if self.size > 0:
-      return self.head.get_node_data()
-    else:
-      print("No orders waiting!")
-  
-  def get_size(self):
-    return self.size
-  
+  def is_empty(self):
+    return self.current_size ==0
+
   def has_space(self):
     if self.max_size == None:
       return True
     else:
-      return self.max_size > self.get_size()
-    
-  def is_empty(self):
-    return self.size == 0
+      return self.max_size > self.current_size
+   
+my_queue = Queue(5)
+my_queue.enqueue("Order #1")
+my_queue.enqueue("Order #2")
+my_queue.enqueue("Order #3")
+my_queue.enqueue("Order #4")
+my_queue.enqueue("Order #5")
+my_queue.dequeue()
+my_queue.dequeue()
+my_queue.dequeue()
+my_queue.dequeue()
+my_queue.dequeue()
+
